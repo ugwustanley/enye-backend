c64707d04e3134863e5710d9df4ee98586b68451
ugwustanley <ugwuchiagoziestanley@gmail.com>
2021-01-15 22:54:36 +0100

    finally. thank God

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use( exchange)
+app.use( "/api/rates", exchange)
 // "/api/rates" ,
 
 
